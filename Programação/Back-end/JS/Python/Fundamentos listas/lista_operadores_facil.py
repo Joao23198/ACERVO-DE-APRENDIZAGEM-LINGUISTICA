81c7ac61039c057344b2ef82de4d2a0177c66fa3
Borges Joao (CaP/ETS2) <bjo4ca@bosch.com>
2026-07-15 09:33:53 -0300

    estudando python, exercicios de operadores

---
+++ Programação/Back-end/JS/Python/Fundamentos listas/lista_operadores_facil.py
@@ -0,0 +1,190 @@
+"Lista um de Operadores"
+
+"1°"
+
+"var_1 = 10"
+
+"var_2 = 20"
+
+"soma = var_1 + var_2"
+
+"print(soma)"
+
+"2°"
+
+"var_3 = 5"
+
+"var_4 = 3"
+
+"diferenca = var_3 - var_4"
+
+"print(diferenca)"
+
+"3°"
+
+"var_5 = 5"
+
+"var_6 = 3"
+
+"produto = var_5 * var_6"
+
+"print(produto)"
+
+"4°"
+
+"var_7 = 8"
+
+"var_8 = 5"
+
+"resultado = var_7 / var_8"
+
+"print(resultado)"
+
+"5°"
+
+"dez = 10"
+
+"tres = 3"
+
+"divi = 10 // 3"
+
+"print(divi)"
+
+"6°"
+
+"qui = 15"
+
+"qua = 4"
+
+"per = qui % qua"
+
+"print(per)"
+
+"7°"
+
+"pot = 2 ** 5"
+
+"print(pot)"
+
+"8°"
+
+"te = 10"
+
+"print(te == 10)"
+
+"9°"
+
+"print( 7 > 3 )"
+
+"10°"
+
+"print(5 > 2 and 10 > 8)"
+
+"----------------------------------------------------------------------------------------------------------------------------------------"
+
+"11°"
+
+# num = int(input("Insira um número: "))
+# if num % 2 == 0:
+#     print("par")
+# else:
+#     print("impar")
+
+"12°"
+
+# numero =  int(input("Insira um numero aqui: "));
+# num = int(input("Insira outro número aqui:"));
+# if numero > num:
+#     print("É Maior")
+# else:
+#     print("Menor")
+"13°"
+
+# nota = float(input("Insira tua nota aqui: "))
+
+# if nota >= 7:
+#     print("Aprovado")
+# elif nota < 7 and nota >= 5:
+#     print("Recuperação")
+# else:
+#     print("Reprovado")
+
+"14°"
+
+# pessoa = int(input("Insira tua idade : "))
+
+# if pessoa >= 18:
+#     print("Pode dirigir")
+# else:
+#     print("Não pode dirigir")
+
+"15°"
+
+# num_1 = int(input("Insira um numero aqui: "))
+
+# num_2 = int(input("Insira um numero aqui: "))
+
+# divisao =  num_1 / num_2
+
+# if divisao > 0 or divisao < 0:
+#     print(" Correto ")
+# else:
+#     print(" Incorreto, algum numero digitado é igual a zero e não existe divisão por zero ")
+
+"16°"
+
+# idade = int(input("Insira tua idade:"))
+
+# if idade in range(18,30):
+#     print("certo")
+# else:
+#     print("errado")
+
+    
+# idade = int(input("Insira tua idade:"))
+
+# if idade >= 18 and  idade <= 30:
+#     print("certo")
+# else:
+#     print("errado")
+
+"17°"
+
+# valor = float(input("Insira o valor da compra aqui: "))
+
+# if valor > 100:
+#     desconto = valor * 0.1
+#     valor_atualizado = valor - desconto
+#     print("Desconto:", valor_atualizado  )
+# else:
+#     print("Normal")
+
+
+"18°"
+
+# multi_cinco = int(input("Insira um numero:"))
+
+# if multi_cinco % 5 == 0:
+#     print("É múltiplo de cinco")
+# else:
+#     print("Não é múltiplo de cinco")
+
+"19°"
+
+# senha = str(input("Insira a senha aqui: "))
+
+# if senha == "python123":
+#     print("senha correta")
+# else:
+#     print("senha incorreta")
+
+"20°"
+
+# numero_int = int(input("Insira um número entre 1 e 100: "))
+
+# if numero_int in range(1,100):
+#     print("Sim")
+# else:
+#     print("Não")
+
+"-----------------------------------------------------------------------"
